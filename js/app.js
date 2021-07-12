fac4040197f9a6528583ec3d698923125f788090
CJ McCaskill <cjmccaskill@gmail.com>
2021-07-12 17:28:45 -0500

    add call function for AJAX, styled hero

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
+const $projects = $("#projects");
+
+// Asynchronous Request for the Data in our JSON
+$.ajax("./json/projects.json")
+  // when the data arrives, do something with it
+  .then((data) => {
+    // loop over the array of the projects
+    data.forEach((project, index) => {
+      const $div = $("<div>");
+      $div.html(`<h3 class ='project-name'>${project.name}</h3>
+        <p class ='project-info'>${project.description}</p>
+        <img src ='${project.image}'/>`);
+
+      $("#projects").append($div); //append the div to the projects section
+    });
+  })
+  .catch((error) => console.log(error));
+
 // Event listner for contact form
